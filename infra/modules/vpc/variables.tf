5e71e7c7be3ef18c33a86c12c870afa85671cff7
Khaled Elshazly <khaledee2002@gmail.com>
2026-03-06 00:14:45 -0500

    Story 2.2: Completed VPC module 3 layered architecture network configurations (task 1), also started RDS module (task 2).

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name of the project, used for resource naming and tagging"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR range for the VPC resource to cover"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "enable_dns_support" {
+  description = "Whether or not DNS support should be enabled in this VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Whethr or not DNS hostnames should be enabled in this VPC"
+  type        = bool
+  default     = true
+}
